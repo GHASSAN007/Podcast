317c71f5cf6b40cf05f47acbb0b2c77462535fc3
GHASSAN007 <simon.ghassan@gmail.com>
2020-12-27 03:15:15 +0200

    💾 Adding channel , comments models

--- podcast/Podcast_Mini_Backend/Podcast/models.py
+++ podcast/Podcast_Mini_Backend/Podcast/models.py
@@ -1,26 +1,66 @@
 from django.db import models
 from django.contrib import auth
-#from .channel.models import channel, Podcaster_info
-#from .comments import comment
 
 
+ENGLISH = 'EN'
+ARABIC = 'AR'
+CHINES = 'ZH'
+DUTCH = 'NL'
+FRENCH = 'FR'
+GERMAN = 'DE'
+RUSSIAN = 'RU'
+JAPANESE = 'JA'
+
+Languages = [
+    (ENGLISH, 'English'),
+    (ARABIC, 'Arabic'),
+    (CHINES, 'Chines'),
+    (DUTCH, 'Dutch'),
+    (FRENCH, 'French'),
+    (GERMAN, 'German'),
+    (RUSSIAN, 'Russian'),
+    (JAPANESE, 'Japanese'),
+]
 
 #class for podcast data 
 class Podcast_info(models.Model):
-
-    Title : str = models.CharField(max_length = 200)                                # Title of the podcast
+    
+    Title : str = models.CharField(max_length = 200, unique=True)                                # Title of the podcast
     Description : str = models.TextField(blank = True , default = "No Description") #description of each podcast / story
     podcast = models.FileField(upload_to = 'podcasts/')                             # the audio file  
-    Thumbnail = models.ImageField(upload_to = 'Thumbnail/')                        #the image of podcast
-    podcasted_by : str = models.CharField(max_length=100)     
-    is_viewed : bool = models.BooleanField(default = False)                         # checking if the user listend to the podcast before 
+    Thumbnail = models.ImageField(upload_to = 'Thumbnail/')                         #the image of podcast
+    posting_date  = models.DateTimeField(auto_now=True)
+    podcasted_by : str = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    channel = models.ForeignKey('channel' , on_delete=models.CASCADE ) 
+    is_viewed : bool = models.BooleanField(default = False)                         # checking if the user listend to the podcast beviewed 
     is_liked : bool = models.BooleanField(default = False)                          # checking if the user liked the podcast before 
     likes  : int = models.IntegerField( default=0 )                                 #number of likes 
     views : int = models.IntegerField(default=0)                                    # number of views 
-    #language : str = models.CharField()
+    language : str = models.CharField(max_length=3, choices=Languages)
 
     def __string__(self):
-        return self.Title  
-    
-    
-    
+        return self.Title
+
+
+
+
+class channel(models.Model):
+	channel_name : str = models.CharField(max_length=250 , unique=True)            
+	podcast_maker = models.ForeignKey('auth.User' , on_delete=models.CASCADE)
+	subscribers : int = models.IntegerField(default=False)
+	creation_date  = models.DateField(auto_now=True)
+	podcast_num : int = models.IntegerField(default=0)
+
+	def __string__(self):
+		return self.channel_name
+
+
+class comment(models.Model):
+    text : str = models.TextField(blank=False)
+    date_of_comment = models.DateTimeField(auto_now=True)
+    podcast = models.ForeignKey(Podcast_info , on_delete = models.CASCADE)
+    user = models.ForeignKey('auth.User' , on_delete = models.CASCADE)
+
+    def __string__(self):
+        return self.text
+
