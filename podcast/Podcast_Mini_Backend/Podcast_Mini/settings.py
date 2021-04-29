1911a61cae3afe40c750063119f3e9a966a45a2f
GHASSAN007 <simon.ghassan@gmail.com>
2021-04-29 23:50:51 +0300

    automate IP process

--- podcast/Podcast_Mini_Backend/Podcast_Mini/settings.py
+++ podcast/Podcast_Mini_Backend/Podcast_Mini/settings.py
@@ -1,12 +1,12 @@
-
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from COMMON.ip_address import get_ip_address 
 
 load_dotenv()
 
-IP_ADDRESS = os.getenv('IP_ADDRESS')
-PORT = os.getenv('PORT')
+IP_ADDRESS =  get_ip_address()
+PORT = '8080'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
