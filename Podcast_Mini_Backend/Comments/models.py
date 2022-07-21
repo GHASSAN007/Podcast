df5bdc830ed428ba13cce37375db787467b40ee6
GHASSAN007 <ghassan.mohder@gmail.com>
2022-07-21 20:07:51 +0300

    🔧fix typo in Comments model file

--- Podcast_Mini_Backend/Comments/models.py
+++ Podcast_Mini_Backend/Comments/models.py
@@ -6,7 +6,7 @@ class comment(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.TextField(blank=False)
     date_of_comment = models.DateTimeField(auto_now=True)
-    podcast = models.ForeignKey('Podcast.Podcast', on_delete=models.CASCADE)
+    podcast = models.ForeignKey('Podcast.podcast', on_delete=models.CASCADE)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     likes = models.PositiveIntegerField(default=0)
     is_podcast_comment_blocked = models.BooleanField(default=False)
