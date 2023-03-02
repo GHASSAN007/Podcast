2c429856223805c0e49e8f322a2cee554713d72a
GHASSAN007 <simon.ghassan@gmail.com>
2023-03-02 23:44:37 +0200

    adding as_view() to the views source

--- Podcast_Mini_Backend/Podcast/urls.py
+++ Podcast_Mini_Backend/Podcast/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview , name='api_overview'),
+    path('', views.api_over_view.as_view() , name='api_over_view'),
     
-    path('podcast-list/' , views.podcast_List , name="podcast_list"),
-    path('podcast-view/<str:pk>/' , views.Podcast_detail , name="podcast_detail"),
-    path('create-podcast/' , views.podcast_create , name="create_podcast"),
-    path('podcast-update/<str:pk>' , views.podcast_update , name= "update_podcast"),
-    path('podcast-delete/<str:pk>' , views.delete_podcast ,name="delete_podcast"),
+    path('podcast-list/' , views.podcast_list.as_view() , name="podcast_list"),
+    path('podcast-view/<str:pk>/' , views.podcast_detail.as_view(), name="podcast_detail"),
+    path('create-podcast/' , views.podcast_create.as_view(), name="create_podcast"),
+    path('podcast-update/<str:pk>' , views.podcast_update.as_view() , name= "update_podcast"),
+    path('podcast-delete/<str:pk>' , views.podcast_delete.as_view() ,name="delete_podcast"),
 ]
