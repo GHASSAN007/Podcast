98bbdcaf98bda0cac8385cb9a65535abe0afc613
GHASSAN007 <ghassan.mohder78@gmail.com>
2023-09-07 19:04:39 +0300

    Updating Urls

--- Podcast_Mini_Backend/Podcast_Mini/urls.py
+++ Podcast_Mini_Backend/Podcast_Mini/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-from Podcast.models import podcast
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('podcast_api/' , include('Podcast.urls')),
     path('stories_api/' , include('stores.urls')),
+    path('comments_api/', include('Comments.urls')),
+    path('channels_api/', include('Channels.urls')),
+    path('location/', include('Geo_Location.urls')),
     path('', include('Front_end.urls'))
 
 ]
