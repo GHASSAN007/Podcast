0b89abd1d0a90532a183d7aaa47db4b1ee9be9b9
GHASSAN007 <ghassan.mohder78@gmail.com>
2023-09-07 19:04:08 +0300

    Updating urls

--- Podcast_Mini_Backend/Podcast/views.py
+++ Podcast_Mini_Backend/Podcast/views.py
@@ -1,114 +1,64 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import status
-from django.http import HttpResponseNotAllowed , Http404
+from django.http import HttpResponseNotAllowed
 
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import podcast_serializer 
 from .models import *
 
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'List_podcast':'/podcast-list/',               
-        'Podcast_Detailview':'/podcast-view/<str:pk>',
-        'Create_podcast':'/create-podcast/',
-        'Update-podcast': '/podcast-update/<str:pk>/',
-        'Delete_podcast': '/podcast-delete/<str:pk>/',
-    }
 
-    return Response(api_urls)
+class api_over_view(APIView):
+    def get(request):
+        api_urls = {
+            'List_podcast':'/podcast-list/',               
+            'Podcast_Detailview':'/podcast-view/<str:pk>',
+            'Create_podcast':'/create-podcast/',
+            'Update-podcast': '/podcast-update/<str:pk>/',
+            'Delete_podcast': '/podcast-delete/<str:pk>/',
+        }
 
-@api_view(['GET'])
-def podcast_List(request):
-    podcasts = podcast.objects.all()
-    serializer = podcast_serializer(podcasts , many=True)
-    return Response(serializer.data)
+        return Response(api_urls)
 
+class podcast_list(APIView):
+    def get(request):
+        podcasts = podcast.objects.all()
+        serializer = podcast_serializer(podcasts , many=True)
+        return Response(serializer.data)
 
-@api_view(['GET'])
-def Podcast_detail(request , pk):
-#    if podcast.is_podcast_blocked:
- #       return HttpResponseForbidden
-#    else:
+class podcast_detail(APIView):
+    def get(request , pk):
         podcast = podcast.objects.get(pk=pk)
         if podcast.is_podcast_blocked:
             return HttpResponseNotAllowed            
         else:
-            serializer = podcast_Serializer(podcast , many=False)
+            serializer = podcast_serializer(podcast , many=False)
 
             return Response(serializer.data)
 
 
-@api_view(['POST'])
-def podcast_create(request):
-    serializer = podcast_Serializer(data=request.data)
+class podcast_create(APIView):
+    def post(self, request):
+        serializer = podcast_serializer(data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
+        if serializer.is_valid():
+            serializer.save()
 
-    return Response(serializer.data)
+        return Response(serializer.data)
 
+class podcast_update(APIView):
+    def podcast_update(request , pk):
+        podcast = podcast.objects.get(pk=pk)
+        serializer = podcast_serializer(instance=podcast, data=request.data)
 
-@api_view(['POST'])
-def podcast_update(request , pk):
-    podcast = podcast.objects.get(pk=pk)
-    serializer = Podcast_Serializer( instance=podcast, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
 
-    if serializer.is_valid():
-        serializer.save()
+        return Response(serializer.data)
+class podcast_delete(APIView):
+    def delete_podcast(request, pk):
+        podcast = podcast.objects.get(pk=pk)
+        podcast.delete()
 
-    return Response(serializer.data)
-
-@api_view(['DELETE'])
-def delete_podcast(request, pk):
-    podcast = podcast.objects.get(pk=pk)
-    podcast.delete()
-
-    return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
  #-------------------------------------------------
-
-
-@api_view(['GET'])
-def comments_List(request):
-    _comments = comment.objects.all()
-    serializer = comment_serializer(_comments , many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def comment_detail(request , pk):
-    _comment = comment.objects.get(pk=pk)
-    serializer = comment_serializer(_comment , many=False)
-
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def comment_create(request):
-    serializer = comment_serializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def comment_update(request , pk):
-    _comment = comment.objects.get(pk=pk)
-    serializer =comment_serializer( instance=_comment, data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-@api_view(['DELETE'])
-def Delete_comment(request, pk):
-    _comment = comment.objects.get(pk=pk)
-    _comment.delete()
-
-    return Response(status=status.HTTP_204_NO_CONTENT)
